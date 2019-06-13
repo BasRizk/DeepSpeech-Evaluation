1be311ae081f8a33bb8e8acd40967f75fb20a3b0
basRizk <basem.rizk@outlook.com>
2019-06-13 18:16:25 +0200

    create models only if it does not exist

--- model_0.4.1.sh
+++ model_0.4.1.sh
@@ -3,6 +3,8 @@
 wget --no-check-certificate https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-models.tar.gz
 mkdir v0.4.1 && tar -xzvf deepspeech-0.4.1-models.tar.gz -C v0.4.1 --strip-components 1
 rm -f deepspeech-0.4.1-models.tar.gz
-mkdir models
+if [ ! -d models ] then
+    mkdir models
+fi
 mv v0.4.1 models
 echo 'Mozilla deepspeech 0.4.1 pre-trained model imported successfully."
--- model_0.5.0.sh
+++ model_0.5.0.sh
@@ -3,6 +3,8 @@
 wget --no-check-certificate https://github.com/mozilla/DeepSpeech/releases/download/v0.5.0/deepspeech-0.5.0-models.tar.gz
 mkdir v0.5.0 && tar -xzvf deepspeech-0.5.0-models.tar.gz -C v0.5.0 --strip-components 1
 rm -f deepspeech-0.5.0-models.tar.gz
-mkdir models
+if [ ! -d models ] then
+    mkdir models
+fi
 mv v0.5.0 models
 echo 'Mozilla deepspeech 0.5.0 pre-trained model imported successfully."
