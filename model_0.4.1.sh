df347327f0bc9e2b7c60450283a1aff0b78d23e4
ironbas3 <basem.rizk@outlook.com>
2019-06-13 19:00:45 +0200

    fix syntax line

--- model_0.4.1.sh
+++ model_0.4.1.sh
@@ -3,8 +3,11 @@
 wget --no-check-certificate https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-models.tar.gz
 mkdir v0.4.1 && tar -xzvf deepspeech-0.4.1-models.tar.gz -C v0.4.1 --strip-components 1
 rm -f deepspeech-0.4.1-models.tar.gz
-if [ ! -d models ] then
+if [ ! -d models ]
+then
     mkdir models
+else
+   echo "models dir already exists"
 fi
 mv v0.4.1 models
-echo 'Mozilla deepspeech 0.4.1 pre-trained model imported successfully."
+echo "Mozilla deepspeech 0.4.1 pre-trained model imported successfully."
--- model_0.5.0.sh
+++ model_0.5.0.sh
@@ -3,8 +3,11 @@
 wget --no-check-certificate https://github.com/mozilla/DeepSpeech/releases/download/v0.5.0/deepspeech-0.5.0-models.tar.gz
 mkdir v0.5.0 && tar -xzvf deepspeech-0.5.0-models.tar.gz -C v0.5.0 --strip-components 1
 rm -f deepspeech-0.5.0-models.tar.gz
-if [ ! -d models ] then
+if [ ! -d models ]
+then
     mkdir models
+else
+   echo "models dir already exists"
 fi
 mv v0.5.0 models
-echo 'Mozilla deepspeech 0.5.0 pre-trained model imported successfully."
+echo "Mozilla deepspeech 0.5.0 pre-trained model imported successfully."
